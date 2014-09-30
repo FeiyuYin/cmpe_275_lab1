ac0cb7748185bcafd49d685304eee3043c79fe84
divyanshi2 <divyanshi.dwivedi11@gmail.com>
2014-09-30 10:06:25 -0700

    Update ProfileServiceImpl.java

--- src/main/java/org/feiyu/cmpe275/Lab1/ProfileServiceImpl.java
+++ src/main/java/org/feiyu/cmpe275/Lab1/ProfileServiceImpl.java
@@ -4,10 +4,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * This class implements the Profileservice interface
+*/
 public class ProfileServiceImpl implements ProfileService{
 	
+	/**
+	 * HashMap named profileMap
+	 * Type of key maintained by this map is String
+	 * Type of mapped value is Set of Profiles
+	*/
+	
 	HashMap<String, Set<Profile>> profileMap; 
-	List<Profile> profiles; 
+	List<Profile> profiles;                                // List is created which saves all profile
 	
 //	public ProfileServiceImpl(HashMap<String, List<Profile>> profiles){
 //		
@@ -34,6 +43,20 @@ public class ProfileServiceImpl implements ProfileService{
 		return profiles; 
 	}
 
+       /** 
+	 
+	* Read the profile of another user or oneself. 
+	 
+	* @param userId the ID of the current user 
+	 
+	* @param profileUserId the ID of user, whose profile is being requested 
+	 
+	* @return the profile for profileUserId   
+	 
+	*/ 
+
+
+
 	public Profile readProfile(String userId, String profileUserId) {
 		// TODO Auto-generated method stub
 		
@@ -46,6 +69,21 @@ public class ProfileServiceImpl implements ProfileService{
 		} 
 		return null;
 	}
+	
+	
+	
+	/** 
+	 
+	* Share a profile with another user. The profile may or may not belong to the current user. 
+	 
+	* @param userId the ID of the current user 
+	 
+	* @param profileUserId the ID of the user, whose profile is being shared 
+	 
+	* @param targetUserId the ID of the user to share the profile with 
+	 
+	*/ 
+	
 
 	public void shareProfile(String userId, String profileUserId, String targetUserId) {
 		// TODO Auto-generated method stub
@@ -59,6 +97,18 @@ public class ProfileServiceImpl implements ProfileService{
 			}
 		}
 	}
+	
+	
+	/** 
+	 
+	* Unshare the current user's own profile with another user. 
+	 
+	* @param userId 
+	 
+	* @param targetUserId 
+	 
+	*/ 
+
 
 	public void unshareProfile(String userId, String targetUserId) {
 		// TODO Auto-generated method stub
